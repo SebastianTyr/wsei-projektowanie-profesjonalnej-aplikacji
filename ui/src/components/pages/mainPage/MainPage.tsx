29df43f8f21a7fc88d7cbaedb9fa84cd4cb63bb6
karolinaJot <amicus.karo@gmail.com>
2021-07-13 22:34:44 +0200

    add login form, add loginPage

--- ui/src/components/pages/mainPage/MainPage.tsx
+++ ui/src/components/pages/mainPage/MainPage.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import MainContent from './MainContent';
 import TopBar from './TopBar';
 import ProfilePage from '../profilePage/ProfilePage';
+import LoginPage from '../loginPage/LoginPage';
 
 const Wrapper = styled.div`
     min-height: 100vh;
@@ -20,6 +21,9 @@ const MainPage: FC = () => {
                 <Switch>
                     <Route path="/profile" exact>
                         < ProfilePage />
+                    </Route>
+                    <Route path="/login" exact>
+                        <LoginPage/>
                     </Route>
                     <Route path="/" exact>
                         <MainContent />
