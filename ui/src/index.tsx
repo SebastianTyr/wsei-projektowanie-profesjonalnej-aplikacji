e37b28e14f9336cea0ea7fb7fc56e00de6f575e0
karolinaJot <amicus.karo@gmail.com>
2021-07-14 22:35:49 +0200

    add files and folders, create global state form Authorization

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './tools/store';
 
 ReactDOM.render(
   <Provider store={store}>
