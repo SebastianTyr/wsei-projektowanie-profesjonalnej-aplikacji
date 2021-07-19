8043d1421604b39cdea878b52c8a0c92a65bc54c
Anna Mika <aniamika90@gmail.com>
2021-07-19 22:55:12 +0200

    fix form styling

--- ui/src/components/announcementForm/announcementForm.tsx
+++ ui/src/components/announcementForm/announcementForm.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { useState } from 'react';
-import { Formik, Form, ErrorMessage } from "formik";
+import { Formik, Form, ErrorMessage, Field } from "formik";
 import * as Yup from 'yup';
 import Button from "../common/Button";
 import Label from "../common/Label";
 import Input from "../common/Input";
 import ErrorBox from "../common/ErrorBox";
 import { Margin } from "../../styledHelpers/Margin";
+import { Colors } from "../../styledHelpers/Colors";
+import { FontSize } from "../../styledHelpers/FontSize";
 
 const Wrapper = styled.div`
    .registration-form__label {
@@ -14,6 +16,7 @@ const Wrapper = styled.div`
    }
    .registration-form__input {
        margin: 0 ${Margin[8]} ${Margin[8]} ${Margin[8]};
+       width: calc(100% - 0.5rem);
    }
    .registration-form__error {
        margin: 0 ${Margin[8]} ${Margin[8]} ${Margin[8]};
@@ -25,9 +28,23 @@ const FormItem = styled.div`
     flex-direction: column;
     margin-bottom: 20px;
 `;
+const SectionHeader = styled.span`
+    font-size: ${FontSize[20]};
+    color: ${Colors.navy};
+    font-weight: 600;
+    text-transform: uppercase;
+    margin: 0 ${Margin[8]} ${Margin[16]} ${Margin[8]};
+`
+const InputContainer = styled.div`
+    margin-top: ${Margin[8]};
+`;
+const SingleItem = styled.div`
+    width: 50%;
+`;
 const ItemsBox = styled.div`
     display: flex;
-`;
+    align-items: center;
+`
 const CustomForm = styled(Form)`
     display: flex;
     flex-direction: column;
@@ -73,6 +90,9 @@ const AnnouncementForm = () => {
 
     return (
         <Wrapper className="modal">
+            <div className="modal__header">
+                <h2>Dodaj wydarzenie</h2>
+            </div>
             <Formik
                 initialValues={initialValues}
                 validationSchema={validationSchema}
@@ -92,36 +112,59 @@ const AnnouncementForm = () => {
                 }}
             >
                 <CustomForm>
-                    <Label labelName="Dodaj wydarzenie" />
+
                     <FormItem>
                         <Label htmlFor='Date' labelName="Data" className="registration-form__label" />
                         <Input id='Date' type='date' name='date' className="registration-form__input" />
                         <ErrorMessage name='date' render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
                     </FormItem>
-                    <ItemsBox>
-                        <Label labelName="Adres" className="registration-form__label" />
+                        <SectionHeader>Adres:</SectionHeader>
                         <FormItem>
-                            <Label htmlFor='address' labelName="Ulica" className="registration-form__label" />
-                            <Input id='address' type='text' name='address.street' className="registration-form__input" />
-                            <ErrorMessage name="address.street" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
-                            <Label htmlFor='city' labelName="Miasto" className="registration-form__label" />
-                            <Input id='city' type='text' name='address.city' className="registration-form__input" />
-                            <ErrorMessage name="address.city" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
-                            <Label htmlFor='postCode' labelName="Kod-Pocztowy" className="registration-form__label" />
-                            <Input id='postCode' type='text' name='address.postCode' className="registration-form__input" />
-                            <ErrorMessage name="address.postCode" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
-                            <Label htmlFor='country' labelName="Kraj" className="registration-form__label" />
-                            <Input id='country' type='text' name='address.country' className="registration-form__input" />
-                            <ErrorMessage name="address.country" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
+                            <ItemsBox>
+                                <SingleItem>
+                                    <Label htmlFor='address' labelName="Ulica" className="registration-form__label" />
+                                    <InputContainer>
+                                        <Input id='address' type='text' name='address.street' className="registration-form__input" />
+                                        <ErrorMessage name="address.street" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
+                                    </InputContainer>
+                                </SingleItem>
+                                <SingleItem>
+                                    <Label htmlFor='city' labelName="Miasto" className="registration-form__label" />
+                                    <InputContainer>
+                                        <Input id='city' type='text' name='address.city' className="registration-form__input" />
+                                        <ErrorMessage name="address.city" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
+                                    </InputContainer>
+                                </SingleItem>
+                            </ItemsBox>
+                            <ItemsBox>
+                                <SingleItem>
+                                    <Label htmlFor='postCode' labelName="Kod pocztowy" className="registration-form__label" />
+                                    <InputContainer>
+                                        <Input id='postCode' type='text' name='address.postCode' className="registration-form__input" />
+                                        <ErrorMessage name="address.postCode" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
+                                    </InputContainer>
+                                </SingleItem>
+                                <SingleItem>
+                                    <Label htmlFor='country' labelName="Kraj" className="registration-form__label" />
+                                    <InputContainer>
+                                        <Input id='country' type='text' name='address.country' className="registration-form__input" />
+                                        <ErrorMessage name="address.country" render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
+                                    </InputContainer>
+                                </SingleItem>
+                            </ItemsBox>
                         </FormItem>
-                    </ItemsBox>
-                    <ItemsBox>
                         <FormItem>
                             <Label htmlFor='description' labelName="Opis" className="registration-form__label" />
-                            <Input id="description" type='textarea' name='description' className="registration-form__input" />
+                            <Field as='textarea'
+                                cols="30" 
+                                rows="5"
+                                id='description'
+                                type='text'
+                                name='description'
+                                className="registration-form__input"
+                            />
                             <ErrorMessage name='description' render={error => <ErrorBox errorText={error} className="registration-form__error" />} />
                         </FormItem>
-                    </ItemsBox>
                     <ButtonWrapper>
                         <Button type='submit' variant="secondary" size="lg" text="Dodaj wydarzenie" />
                     </ButtonWrapper>
