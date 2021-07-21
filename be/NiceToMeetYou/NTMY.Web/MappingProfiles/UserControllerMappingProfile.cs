d64c9f0fa2d26a69d1706f90a1c09f324e7c2f35
Jakub Święch <jakub.swiech@kotrak.com>
2021-07-21 21:45:57 +0200

    Add error handler

--- be/NiceToMeetYou/NTMY.Web/MappingProfiles/UserControllerMappingProfile.cs
+++ be/NiceToMeetYou/NTMY.Web/MappingProfiles/UserControllerMappingProfile.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using NTMY.Application.Interfaces.Users.DTOs;
 using NTMY.Web.ViewModels.Users;
 
