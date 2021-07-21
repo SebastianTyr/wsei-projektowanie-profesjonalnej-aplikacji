60c3847f28204504f2aba048581c40d2b41cc8fa
Anna Mika <aniamika90@gmail.com>
2021-07-21 15:12:43 +0200

    profile page basic styling

--- ui/src/components/pages/profilePage/ProfilePage.tsx
+++ ui/src/components/pages/profilePage/ProfilePage.tsx
@@ -9,8 +9,64 @@ import { useSelector } from 'react-redux';
 import { IState } from '../../../reducers';
 import { ILoggedInReducer } from '../../../reducers/loggedInUserReducers';
 import { ICurrentUserDetailsReducers } from '../../../reducers/currentUserDetailsReducers';
+import { Padding } from '../../../styledHelpers/Padding';
+import { Gradient } from '../../../styledHelpers/Gradient';
+import { Colors } from '../../../styledHelpers/Colors';
 
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 76px);
+    width: 100vw;
+    background-image: ${Gradient.orangePink};
+`
+const WrapperContainer = styled.div`
+    background-color: ${Colors.white};
+    width: 90%;
+    height: 90%;
+    display: flex;
+    align-items: center;
+    border-radius: 1rem;
+    box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+`
+const ImageContainer = styled.div`
+    display: flex;
+    width: 40%;
+    padding: ${Padding[16]};
+    height: 100%;
+`
+const MainContainer = styled.div`
+    width: 60%;
+    padding: ${Padding[16]};
+    height: 100%;
+`
+const MainContainerHeader = styled.div`
+    width: 100%;
+    height: 30%;
+    display: flex;
+    justify-content: space-between;
+`
+const PersonInfo = styled.div`
 
+`
+const Names = styled.p`
+    color: ${Colors.red};
+    font-size: ${FontSize[24]};
+    font-weight: 600;
+`
+const Ranking = styled.div`
+    color: ${Colors.red};
+    font-size: ${FontSize[24]};
+`
+const Description = styled.p`
+    font-size: 1.13rem;
+    color: ${Colors.navy};
+    font-weight: 500;
+`
+const MainContainerBox = styled.div`
+    height: 70%;
+`
 const EditButtonBox = styled.span`
     width: auto;
     height: 50px;
@@ -30,24 +86,35 @@ const ProfilePage = () => {
         setIsEditDeatils(!isEditDetails);
     }
     return (
-        <>
+       <Wrapper>
+           <WrapperContainer>
             {isEditDetails ? (
                 <DetailsForm />
             ) : (
-                <div>
-                    Strona profilu - widok bez formularzy
-                    <div>
-                        <span>Witaj na swoim profilu, {currentUserDetails?.firstName}</span>
-                    </div>
-                    <EditButtonBox>
-                        <Button variant="primary" type="text" size="md" text="Edytuj szczegóły" onClick={editDetailsHandler}></Button>
-                    </EditButtonBox>
-                    <ImageForm/>
-                </div>
+                <>
+                    <ImageContainer>
+                        <ImageForm/>
+                    </ImageContainer>
+                    <MainContainer>
+                        <MainContainerHeader>
+                            <PersonInfo>
+                                <Names>{currentUserDetails?.firstName} {currentUserDetails?.secondName}</Names>
+                               <Description>Description{currentUserDetails?.description}</Description> 
+                               <Ranking></Ranking>
+                            </PersonInfo>
+                            <EditButtonBox>
+                                <Button variant="primary" type="text" size="md" text="Edytuj szczegóły" onClick={editDetailsHandler}></Button>
+                            </EditButtonBox>
+                        </MainContainerHeader>
+                        <MainContainerBox>
+                            About, Wesela
+                        </MainContainerBox>
+                    </MainContainer>
 
+                </>
             )}
-        </>
-
+            </WrapperContainer>
+        </Wrapper>
     );
 };
 
