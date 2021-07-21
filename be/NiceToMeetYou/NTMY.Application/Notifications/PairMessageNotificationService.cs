64a125a1a9e5d68d4fe035ca681c0975195b7ef8
Jakub Święch <jakub.swiech@kotrak.com>
2021-07-21 23:10:57 +0200

    add notification about new pair

--- be/NiceToMeetYou/NTMY.Application/Notifications/PairMessageNotificationService.cs
+++ be/NiceToMeetYou/NTMY.Application/Notifications/PairMessageNotificationService.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.SignalR;
 using NTMY.Application.Interfaces.Notifications;
 using NTMY.Application.Notifications.Hubs;
 using NTMY.Application.Notifications.Responses;
+using NTMY.Domain.Users.Repositories;
 using PlaygroundShared.Domain;
 using PlaygroundShared.Infrastructure;
 
@@ -13,11 +14,13 @@ namespace NTMY.Application.Notifications
     {
         private readonly IHubContext<PairMessagesHub> _hubContext;
         private readonly ICorrelationContext _correlationContext;
+        private readonly IUserRepository _userRepository;
 
-        public PairMessageNotificationService(IHubContext<PairMessagesHub> hubContext, ICorrelationContext correlationContext)
+        public PairMessageNotificationService(IHubContext<PairMessagesHub> hubContext, ICorrelationContext correlationContext, IUserRepository userRepository)
         {
             _hubContext = hubContext ?? throw new ArgumentNullException(nameof(hubContext));
             _correlationContext = correlationContext ?? throw new ArgumentNullException(nameof(correlationContext));
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
         }
 
         public async Task SendMessageAsync(AggregateId toUserId, AggregateId pairId, string message)
@@ -29,6 +32,21 @@ namespace NTMY.Application.Notifications
                 PairId = pairId.ToGuid(),
                 ToUserId = toUserId.ToGuid()
             });
+        }
+
+        public async Task SendPairCreatedNotificationAsync(AggregateId pairId, AggregateId firstUserId, AggregateId secondUserId)
+        {
+            var firstUser = await _userRepository.GetAsync(firstUserId);
+            var secondUser = await _userRepository.GetAsync(secondUserId);
+            await _hubContext.Clients.Users(firstUserId.ToString(), secondUserId.ToString()).SendAsync("pairCreated",
+                new PairCreatedResponse()
+                {
+                    PairId = pairId.Id,
+                    FirstUserFirstName = firstUser?.FirstName,
+                    FirstUserId = firstUserId.Id,
+                    SecondUserFirstName = secondUser?.FirstName,
+                    SecondUserId = secondUserId.Id
+                });
         }
     }
 }
---
+++ be/NiceToMeetYou/NTMY.Application/Notifications/Responses/PairCreatedResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NTMY.Application.Notifications.Responses
+{
+    public class PairCreatedResponse
+    {
+        public Guid PairId { get; set; }
+        public Guid FirstUserId { get; set; }
+        public string FirstUserFirstName { get; set; }
+        public Guid SecondUserId { get; set; }
+        public string SecondUserFirstName { get; set; }
+    }
+}
