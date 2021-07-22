095d2d88e5f6d9058a9d2f9619bc257a320bf167
karolinaJot <amicus.karo@gmail.com>
2021-07-22 18:41:54 +0200

    file format

--- ui/src/components/pages/weddingsPage/AnnouncementContent.tsx
+++ ui/src/components/pages/weddingsPage/AnnouncementContent.tsx
@@ -13,20 +13,20 @@ const CardWrapper = styled.div`
     flex-wrap: wrap;
 `;
 
-interface Iweddings{
-  firstName:string,
+interface Iweddings {
+  firstName: string,
   description: string,
   date: Date,
   addressStreet: string,
   addressCity: string,
   addressPostCode: string,
   addressCountry: string,
-  weddingNo:number
+  weddingNo: number
 }
 
 const Announcement = () => {
   const [allAnnouncement, setAllAnouncement] = useState<Iweddings[]>([]);
-  const { incomingWeddingDetails, currentUserDetails } = useSelector<IState, IIncomingWeddingDetailsReducers & ICurrentUserDetailsReducers>( state => ({
+  const { incomingWeddingDetails, currentUserDetails } = useSelector<IState, IIncomingWeddingDetailsReducers & ICurrentUserDetailsReducers>(state => ({
     ...state.incomingWeddingDetails,
     ...state.currentUserDetails
   }));
@@ -40,39 +40,39 @@ const Announcement = () => {
 
   // const urlSelectedUsers = `https://localhost:5001/Users/GetIncomingWeddings?${usersParams}`;
   const urlSelectedUsers = 'https://localhost:5001/Users/GetIncomingWeddings';
- 
+
 
   useEffect(() => {
     fetch(urlSelectedUsers, {
       method: 'GET',
       headers: { "Authorization": "Bearer " + sessionStorage.getItem('jwtToken') }
     })
-    .then(response => response.json())
-    .then((data => {
+      .then(response => response.json())
+      .then((data => {
         console.log(data);
         setAllAnouncement(data.items);
       }));
   }, []);
 
-  return(
+  return (
     <CardWrapper>
       {console.log(incomingWeddingDetails)}
-  <h2>Nadchodzące wydarzenia</h2>
-  {
-    allAnnouncement?.map((props?) => {
-      return(
-        <AnnoucementCard key={props.weddingNo}
-          date={props.date.toLocaleString().substring(0,10)}
-          firstname={props.firstName}
-          description={props.description}
-          street={(props.addressStreet === null) ? 'Nie wiadomo gdzie ':props.addressStreet}
-          city={(props.addressCity === null) ? 'Nie wiadomo gdzie ':props.addressCity}
-          postCode={(props.addressPostCode === null) ? 'Nie wiadomo gdzie ':props.addressPostCode}
-          country={(props.addressCountry === null) ? 'Nie wiadomo gdzie ':props.addressCountry}
-        />);
-  })
-}
-</CardWrapper>
+      <h2>Nadchodzące wydarzenia</h2>
+      {
+        allAnnouncement?.map((props?) => {
+          return (
+            <AnnoucementCard key={props.weddingNo}
+              date={props.date.toLocaleString().substring(0, 10)}
+              firstname={props.firstName}
+              description={props.description}
+              street={(props.addressStreet === null) ? 'Nie wiadomo gdzie ' : props.addressStreet}
+              city={(props.addressCity === null) ? 'Nie wiadomo gdzie ' : props.addressCity}
+              postCode={(props.addressPostCode === null) ? 'Nie wiadomo gdzie ' : props.addressPostCode}
+              country={(props.addressCountry === null) ? 'Nie wiadomo gdzie ' : props.addressCountry}
+            />);
+        })
+      }
+    </CardWrapper>
   );
 }
 export default Announcement;
