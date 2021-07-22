5b7d46608bf37f1e5c420043836bb9e0c3c68ddd
karolinaJot <amicus.karo@gmail.com>
2021-07-23 01:49:53 +0200

    dealing with chat -full of erros

--- ui/src/components/pages/messagePage/MessagePage.tsx
+++ ui/src/components/pages/messagePage/MessagePage.tsx
@@ -1,42 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { IPair, IPairMessage } from "../../../entities/pair";
+import { Formik, Form, ErrorMessage } from "formik";
 import styled from "styled-components";
+import * as signalR from '@microsoft/signalr';
+
+import { IPair, IPairMessage } from "../../../entities/pair";
 import { Padding } from "../../../styledHelpers/Padding";
 import { FontSize } from "../../../styledHelpers/FontSize";
 import { Colors } from "../../../styledHelpers/Colors";
 import { Margin } from "../../../styledHelpers/Margin";
-import * as signalR from '@microsoft/signalr';
 
-const MessagePage = () => {
-    const [pairs, setPairs] = useState<IPair[]>([]);
-    const [currentMessages, setCurrentMessages] = useState<IPairMessage[]>([]);
-    let [currentPair, setCurrentPair] = useState<IPair>();
-    const url = `https://localhost:5001/Pairs`;
-    
-    function setPair(pair: IPair) {
-        setCurrentPair(pair);
-        getPairMessages(pair);
-    }
 
-    function getPairMessages(pair: IPair) {
-        console.log(currentPair);
-        fetch(`${url}/${pair?.pairId}/messages`, {
-            method: 'GET',
-            headers: { "Authorization": "Bearer " + sessionStorage.getItem('jwtToken') }
-        })
-        .then(response => response.json())
-        .then((data: {items: IPairMessage[]}) => {
-            setCurrentMessages(data.items.reverse());
-            console.log(currentMessages);
-        });
-    }
-
-    function addMessageToCollection(message: IPairMessage) {
-        console.log(message);
-        setCurrentMessages(previousState => [...previousState, message]);
-    }
-
-    const Wrapper = styled.div`
+const Wrapper = styled.div`
     width: 70%;
     margin: 0 auto;
     overflow: auto;
@@ -51,12 +25,46 @@ const MessagePage = () => {
     }
 `;
 
+const MessagePage = () => {
+
+    const [pairs, setPairs] = useState<IPair[]>([]);
+    const [currentMessages, setCurrentMessages] = useState<IPairMessage[]>([]);
+    let [currentPair, setCurrentPair] = useState<IPair>();
+    // const url = `https://localhost:5001/Pairs`;
+    const url ='https://localhost:5001/Pairs'
+
+    function setPair(pair: IPair) {
+        setCurrentPair(pair);
+        getPairMessages(pair);
+    }
+
+    function getPairMessages(pair: IPair) {
+        console.log(currentPair);
+        fetch(`${url}/${pair?.pairId}/messages`, {
+            method: 'GET',
+            headers: { "Authorization": "Bearer " + sessionStorage.getItem('jwtToken') }
+        })
+            .then(response => response.json())
+            .then((data: { items: IPairMessage[] }) => {
+                setCurrentMessages(data.items.reverse());
+                console.log(currentMessages);
+            });
+    }
+
+    function addMessageToCollection(message: IPairMessage) {
+        console.log(message);
+        setCurrentMessages(previousState => [...previousState, message]);
+    }
+
+
+
     useEffect(() => {
         const hubConnection: signalR.HubConnection = new signalR.HubConnectionBuilder().withUrl('https://localhost:5001/pairMessages', {
-            accessTokenFactory: () => String(sessionStorage.getItem('jwtToken'))}).build();
+            accessTokenFactory: () => String(sessionStorage.getItem('jwtToken'))
+        }).build();
         hubConnection.start()
-        .then(() => console.log('Connection started'))
-        .catch(err => console.log(err));
+            .then(() => console.log('Connection started'))
+            .catch(err => console.log(err));
 
         hubConnection.on('messageArrived', (data: IPairMessage) => addMessageToCollection(data));
 
@@ -64,48 +72,78 @@ const MessagePage = () => {
             method: 'GET',
             headers: { "Authorization": "Bearer " + sessionStorage.getItem('jwtToken') }
         })
-        .then(response => response.json())
-        .then((data: {items: IPair[]}) => {
-            console.log(data);
-            setPairs(data.items);
-            console.log(pairs)
-        });
+            .then(response => response.json())
+            .then((data: { items: IPair[] }) => {
+                console.log(data);
+                setPairs(data.items);
+                console.log(pairs)
+            });
+
+
+        getPairMessages(pairs)
+
     }, [])
 
     return (
         <>
-        <div className="left-panel"> Lewy panel
-            {
-            pairs.map((pair) => { return (
-                <Wrapper onClick={() => setPair(pair)}>
-                    <div>
-                    Para z: {pair.likedUserFirstName}
-                    </div>
-                    <div>
-                    Ostatnia wiadomość: {pair.lastMessage}
-                    </div>
-                    <hr></hr>
+            <div className="left-panel"> Lewy panel
+                {
+                    pairs.map((pair) => {
+                        return (
+                            <Wrapper onClick={() => setPair(pair)}>
+                                <div>
+                                    Para z: {pair.likedUserFirstName}
+                                </div>
+                                <div>
+                                    Ostatnia wiadomość: {pair.lastMessage}
+                                </div>
+                                <hr></hr>
+                            </Wrapper>
+                        )
+                    })
+                }
+            </div>
+            <div className="right-panel"> Prawy panel
+                {
+                    currentMessages.map((message) => {
+                        return (
+                            <Wrapper>
+                                <div>Od: {message.fromUserId}</div>
+                                <div>Wiadomość: {message.message}</div>
+                            </Wrapper>
+                        )
+                    })
+                }
+                <Wrapper>
+                    <Formik
+                        initialValues={{ message: 'test' }}
+                        onSubmit={values => {
+                            console.log(values)
+                            const messageData = {
+                                pairId: currentPair?.pairId,
+                                toUserId: currentPair?.likedUserId,
+                                message: values.message
+                            };
+                            console.log(messageData);
+
+                            fetch('https://localhost:5001/Pairs/messages', {
+                                method: 'POST',
+                                headers: { "Content-Type": "application/json",
+                                "Authorization": "Bearer " + sessionStorage.getItem('jwtToken'), },
+                                body: JSON.stringify(messageData)
+                            })
+                            .then((response) => console.log(response))
+                        }
+                        }
+                    >
+                        <Form>
+                            <label htmlFor="message">Wpisz wiadomość: </label>
+                            <input name="message" id="message" type="text"></input>
+                            <button type="submit">Wyślij</button>
+                        </Form>
+                    </Formik>
                 </Wrapper>
-            )})
-        }
-        </div>
-        <div className="right-panel"> Prawy panel
-            {
-                currentMessages.map((message) => {
-                    return (
-                        <Wrapper>
-                            <div>Od: {message.fromUserId}</div>
-                            <div>Wiadomość: {message.message}</div>
-                        </Wrapper>
-                    )
-                })
-            }
-            <Wrapper>
-                    <label>Wpisz wiadomość: </label>
-                    <input name="message" type="text"></input>
-                    <button type="submit">Wyślij</button>
-            </Wrapper>
-        </div>
+            </div>
         </>
     )
 };
